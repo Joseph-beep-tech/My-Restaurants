0bbcdff30519a2c59f4fcbff8fa9b69d97890898
Joseph-beep-tech <Josephcluff006@gmail.com>
2021-01-11 18:32:51 +0300

    Drop files from gitignore

--- app/src/main/java/com/moringaschool/myrestaurants/MainActivity.java
+++ app/src/main/java/com/moringaschool/myrestaurants/MainActivity.java
@@ -14,9 +14,9 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    public static final String TAG = MainActivity.class.getSimpleName();
+
     @BindView(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
     @BindView(R.id.locationEditText) EditText mLocationEditText;
     @BindView(R.id.appNameTextView) TextView mAppNameTextView;
@@ -28,15 +28,25 @@ public class MainActivity extends AppCompatActivity {
        ButterKnife.bind(this);
 
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                String location = mLocationEditText.getText().toString();
+                if (v == mFindRestaurantsButton) {
+                    String location = mLocationEditText.getText().toString();
 //                Log.d(TAG, location);
-                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
-                intent.putExtra("location", location);
-                startActivity(intent);
-                Toast.makeText(MainActivity.this, location, Toast.LENGTH_LONG).show();
+
+                    Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                    intent.putExtra("location", location);
+
+                    startActivity(intent);
+//                Toast.makeText(MainActivity.this, location, Toast.LENGTH_LONG).show();
+                }
             }
         });
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
